e77b7dcb09e30661d977bd53d6d91129aad0b7d4
bmehler <bernhard.mehler@gmail.com>
2017-10-27 23:46:30 +0200

    Initial Video

--- assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-        } else {
-            $(".navbar-fixed-top").removeClass("top-nav-collapse");
-        }
-    });
-});
